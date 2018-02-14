dcf7b2d6dc3f0f4a1918b914fe2e0f255eeeb5ed
Adam Noone <you@example.com>
2018-02-14 16:23:10 +0000

    2nd commit

---
+++ java/ie/dit/Tune.java
@@ -0,0 +1,21 @@
+package ie.dit;
+
+
+public class Tune
+{
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+
+
+
+public void EachTune(int x,String title,String altTitle,String notation)
+{
+	this.x = x;
+	this.title = title;
+	this.altTitle = altTitle;
+	this.notation = notation;
+	
+}
+}
